57ea1a2ed33381219f7b55495d5df5171303e7df
Guillain KALISA <kalisa2002guillain@gmail.com>
2026-03-25 12:32:58 +0200

    Configure pymysql to work as MySQLdb

--- willystore/settings.py
+++ willystore/settings.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 from pathlib import Path
 import os
